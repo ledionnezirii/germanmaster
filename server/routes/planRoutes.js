fba0720b313b41500d9a3495cf01d338bd215df5
Ledion Neziri <ledion.678@gmail.com>
2025-07-29 00:42:39 +0200

    Updates

--- server/routes/planRoutes.js
+++ server/routes/planRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { protect } = require("../middleware/authMiddleware")
+const  protect  = require("../middleware/auth")
 const { createPlan, getPlanByLevel, markTopicAsCompleted } = require("../controllers/planController")
 
 const router = express.Router()
