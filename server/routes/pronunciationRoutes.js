597957fbb41ca9eb82dee8ff8965dffb3a85996d
Ledion Neziri <ledion.678@gmail.com>
2025-09-19 02:24:33 +0200

    Bug fixed at Pronunciation

--- server/routes/pronunciationRoutes.js
+++ server/routes/pronunciationRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getWords, addWord, checkPronunciation } = require("../controllers/pronunciationController");
+const { getWords, addWord, checkPronunciation,getUserCompletedPackages } = require("../controllers/pronunciationController");
 const  protect  = require("../middleware/auth"); // middleware për JWT
 const isAdmin = require("../middleware/isAdmin")
 
 router.get("/", protect, getWords);
 router.post("/", protect,isAdmin, addWord);
 router.post("/check", protect, checkPronunciation);
+router.get("/completed-pronunciation-packages", protect, getUserCompletedPackages)
 
 
 
 module.exports = router;
+
