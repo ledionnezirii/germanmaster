43330f5be5afa0bf86367e2d1a6d7130b8965e43
Ledion Neziri <ledion.678@gmail.com>
2025-09-21 23:21:00 +0200

    Redirects created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -53,7 +53,7 @@ const AppContent = () => {
               <Route path="/signup" element={<SignUp />} />
               <Route path="/forgotpassword" element={<ForgotPassword />} />
               <Route path="resetpassword" element={<ResetPassword />} />
-              <Route path="verifyemail/:token" element={<VerifyEmail />} />
+              <Route path="/verifyemail/:token" element={<VerifyEmail />} />
               {/* <Route path="/challenge" element={<Challenge />} /> */}
               <Route path="/plan" element={<Plan />} />
               <Route path="tests" element={<Tests />} />
