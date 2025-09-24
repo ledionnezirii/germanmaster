f38a29a0e69ab44fc8d5886fbea61cfc6040629d
Ledion Neziri <ledion.678@gmail.com>
2025-09-24 04:31:30 +0200

    Logo fixed at signin

--- client/src/auth/SignUp.jsx
+++ client/src/auth/SignUp.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
-import webLogo from "../images/logoIMG.jpeg"
+import webLogo from "../../public/gjuhagjermanelogo.png"
 import { LockIcon, Mail, User } from 'lucide-react';
 
 
