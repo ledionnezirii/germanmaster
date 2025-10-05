1551ce03a83502ac932bf5e94382e0928808a804
Ledion Neziri <ledion.678@gmail.com>
2025-10-05 05:05:47 +0200

    Email sending bug fixed

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,19 +2,18 @@ const SibApiV3Sdk = require('@sendinblue/client');
 require('dotenv').config();
 
 // Initialize client
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
-const apiKeyAuth = defaultClient.authentications['api-key'];
-apiKeyAuth.apiKey = process.env.BREVO_API_KEY;
-
 const client = new SibApiV3Sdk.TransactionalEmailsApi();
 
+// Set API key
+client.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
+
 const sendEmail = async ({ to, subject, htmlContent }) => {
-  const emailData = new SibApiV3Sdk.SendSmtpEmail({
+  const emailData = {
     sender: { email: process.env.EMAIL_FROM, name: "GjuhëGjerma App" },
     to: [{ email: to, name: "User" }],
     subject,
     htmlContent,
-  });
+  };
 
   try {
     const res = await client.sendTransacEmail(emailData);
