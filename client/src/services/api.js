a1930ee5ae3c55ee521621f0d3e1db054d7e3dc4
Ledion Neziri <ledion.678@gmail.com>
2025-10-21 16:23:06 +0200

    New puuzzle.jsx esing

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = "https://gjuhagjermaneserver.onrender.com/api"
+const API_BASE_URL = "/api"
 export const SOCKET_URL = "https://gjuhagjermaneserver.onrender.com"
 
 export const getAbsoluteImageUrl = (path) => {
